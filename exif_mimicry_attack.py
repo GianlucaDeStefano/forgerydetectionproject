44f329c1281532bfe16ef3242c55c14fe3470507
Gianluca De Stefano <gianluca.de-stefano@cispa.de>
2023-01-12 19:31:17 +0100

    Created exif_mimicry_attack.py

---
+++ exif_mimicry_attack.py
@@ -0,0 +1,39 @@
+import argparse
+import os
+
+from Attacks.Exif.Mimicking.ExifMimickingIntelligentAttack import ExifIntelligentAttack
+from Datasets.ColumbiaUncompressed.ColumbiaUncompressedDataset import ColumbiaUncompressedDataset
+from Datasets.DSO.DsoDataset import DsoDataset
+from Utilities.Experiments.Attacks.MimicryExperiment import MimicryExperiment
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True)
+parser.add_argument('--gpu', required=False, default=None)
+args = parser.parse_known_args()[0]
+
+if args.gpu is not None:
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpu)
+from Utilities.Confs.Configs import Configs
+
+
+dataset_name = args.dataset
+configs = Configs("config.yaml", f"Attack-Noiseprint-{dataset_name}")
+
+if dataset_name == 'DSO':
+    dataset = DsoDataset(configs["global"]["datasets"]["root"])
+elif dataset_name == 'Columbia':
+    dataset = ColumbiaUncompressedDataset(configs["global"]["datasets"]["root"])
+else:
+    raise Exception('Unknown Dataset')
+
+samples = [sample_path for sample_path in dataset.get_forged_images()]
+
+experiment = MimicryExperiment(ExifIntelligentAttack(50, 5, plot_interval=-1, verbosity=0,
+                                                     debug_root=configs.create_debug_folder("executions")),
+                                    configs.create_debug_folder("outputs"), samples, dataset)
+
+experiment.process()
+
+
+
